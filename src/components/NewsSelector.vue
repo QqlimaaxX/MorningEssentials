556a70b16d5378dedfd8ad543a90ea873b9021a7
Mohit Karangiya <mohitkarangiya@gmail.com>
2017-06-09 23:14:22 +0530

    Added Transistions

---
+++ src/components/NewsSelector.vue
@@ -0,0 +1,26 @@
+<template>
+<transition name="bottom-to-up" appear>
+	<div class="ui container segment" @click="selected">
+		<div class="ui header">News</div>
+		<div class="content">Get to know some news from more than 70 newspapers!!</div>
+	</div>
+</transition>
+</template>
+
+<script>
+
+export default{
+	methods:{
+		selected(){
+			this.$emit('newsSelected');
+		}
+	}
+}
+
+</script>
+
+<style scoped>
+	.ui.container.segment{
+		cursor: pointer;
+	}
+</style>
